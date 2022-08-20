efb8995d173b2fd5ffe1c2ef82adfc7296314e67
Wellington Scaburri <wscaburri@gmail.com>
2022-08-19 21:23:30 -0300

    Adicionado validação para RacaViewModel - Refatorado para utilizar comandos asp-helpers no cshtml

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -42,13 +42,22 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
 
             ViewBag.Especies = especies;
 
-            return View();
+            var racaCadastrarViewModel = new RacaCadastrarViewModels();
+
+            return View(racaCadastrarViewModel);
         }
 
         [HttpPost("/raca/cadastrar")]
         public IActionResult Cadastrar(
             [FromForm] RacaCadastrarViewModels racaCadastrarViewModels)
         {
+            // Valida o parâmetro recebido na Action se é invalido
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Especies = ObterEspecies();             
+
+                return View(racaCadastrarViewModels);
+            }
             _racaServico.Cadastrar(racaCadastrarViewModels);
 
             return RedirectToAction("Index");
